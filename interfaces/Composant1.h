6136dc7145ad7a37fd9f7ab86db62094abfe1198
Kounda <72969035+Kounda@users.noreply.github.com>
2023-04-17 15:42:19 +0200

    Update Composant1.h

--- interfaces/Composant1.h
+++ interfaces/Composant1.h
@@ -1,9 +1,9 @@
-#ifndef COMPOSANT1_H
-#define COMPOSANT1_H
+ifndef COMPOSANT1_H
+define COMPOSANT1_H
 
 //extern "C"
 int composant1(int p1, int p2);
 
 const char * getComposant1Version(void);
 
-#endif
+endif
