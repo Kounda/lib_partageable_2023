15ee73fce50db9e78e19a052bc61a235d0c78ee6
Kounda <72969035+Kounda@users.noreply.github.com>
2023-04-18 11:10:35 +0200

    Update Composant2.h

--- interfaces/Composant2.h
+++ interfaces/Composant2.h
@@ -1,9 +1,10 @@
-ifndef COMPOSANT2_H
-define COMPOSANT2_H
+#ifndef COMPOSANT2_H
+#define COMPOSANT2_H
 
 // interface du composant2
 
 
-//extern "C"
+extern "C"
 int composant2(int p1, int p2);
-endif
+
+#endif
